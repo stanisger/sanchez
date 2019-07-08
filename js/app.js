56b12e6d4fa57499cf6a047a8dff24cfe4dbbc49
Apple <gersigner@hotmail.com>
2019-07-07 23:01:03 -0500

    add iner 2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+
+
+var distance = $('.tabbed-search').offset().top - 81 ;
+
+$(window).scroll(function() {
+    if ( $(this).scrollTop() >= distance ) {
+        $('.tabbed-search').addClass('fixed_aside');
+    } else {
+        $('.tabbed-search').removeClass('fixed_aside');
+    }
+});
