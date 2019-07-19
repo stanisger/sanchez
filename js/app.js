67879d11dff65b005128cd78acd3ca0de0fb41da
Apple <gersigner@hotmail.com>
2019-07-18 23:03:25 -0500

    add we are

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ $(document).foundation()
 
 var distance = $('.tabbed-search').offset().top - 81 ;
 
-$(window).scroll(function() {
-    if ( $(this).scrollTop() >= distance ) {
-        $('.tabbed-search').addClass('fixed_aside');
-    } else {
-        $('.tabbed-search').removeClass('fixed_aside');
-    }
-});
+// $(window).scroll(function() {
+//     if ( $(this).scrollTop() >= distance ) {
+//         $('.tabbed-search').addClass('fixed_aside');
+//     } else {
+//         $('.tabbed-search').removeClass('fixed_aside');
+//     }
+// });
